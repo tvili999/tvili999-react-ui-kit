1ca42e346dd5a76358114841171fab96025fa75b
Takács Vili <tvili999@gmail.com>
2022-02-03 23:46:41 +0100

    snapshot

--- src/utils/functional.js
+++ src/utils/functional.js
@@ -7,6 +7,40 @@ const functional = {
                 return true;
         return false;
     },
+    mapObj: (obj, fn) => {
+        return Object.keys(obj)
+            .reduce(
+                (o, key) => {
+                    o[key] = fn(obj[key], key);
+                    return o;
+                },
+                {}
+            )
+    },
+    entries: (obj) => {
+        return Object.keys(obj).map(key => (({ key, value: obj[key] })));
+    },
+    group: (arr, keySupplier) => {
+        return arr.reduce((obj, val) => {
+            const key = keySupplier(val);
+            if(!obj[key])
+                obj[key] = [];
+            obj[key].push(val);
+            return obj;
+        }, {});
+    },
+    merge: (original, merged) => {
+        if(Array.isArray(original) && Array.isArray(merged))
+            return [...original, ...merged];
+
+        if(typeof original === "object" && typeof merged === "object")
+            return functional.mapObj(original, (value, key) => merged.hasOwnProperty(key) ? 
+                functional.merge(value, merged[key]) : 
+                value
+            )
+        
+        return merged;
+    },
     replaceIndex: (array, index, value) => {
         array = array || []
         return array.map((x, i) => i === index ? value : x);
